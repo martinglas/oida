5034090b70cd201466e814f99ee7466b0d376cd9
martin.glas <martin.k.glas@gmail.com>
2016-12-27 23:19:24 +0100

    Added test cases with procedure as comments

---
+++ oida.bridge.test/src/oida/bridge/test/TestModelOntologyGenerator.java
@@ -0,0 +1,21 @@
+package oida.bridge.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestModelOntologyGenerator {
+
+	@Test
+	public void test() {
+		//Tool ontology has been loaded into ontology manager on startup.
+
+		//Notification ADD object named “leftWing”
+		//Get ontology name from project container (Assumption: objects can only be part of one project)
+		//Get OntologyClass SystemElement of ModelClass of leftWing and create Individual leftWing as instance of SystemElement Class
+
+		
+		fail("Not yet implemented");
+	}
+
+}
