8b9429da04db1b9df2f6453e994fccebd24d5cb0
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-01-13 17:04:19 +0100

    project refactoring: created oida.bridge plug-in; moved content of oida.bridge.test to oida.bridge; removed deprectated ontology model and the oida.bridge.test;

--- depr/oida.ontology.model/src/oida/ontology/manager/IOntologyManagerClient.java
+++
@@ -1,7 +0,0 @@
-package oida.ontology.manager;
-
-import org.eclipse.core.databinding.observable.list.ObservableList;
-
-public interface IOntologyManagerClient {
-	public ObservableList<oida.ontologyMgr.LocalOntology> getOntologyList();
-}
--- oida.bridge/test/oida/bridge/test/TestPluginTest.java
+++ oida.bridge/test/oida/bridge/test/TestPluginTest.java
@@ -4,12 +4,12 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import oida.ontology.service.OIDAOntologyService;
+import oida.ontology.service.IOIDAOntologyService;
 
 public class TestPluginTest {
 	
 	@Test
-	public void test(OIDAOntologyService ontologyService) {
+	public void test(IOIDAOntologyService ontologyService) {
 		
 		TotalObserver observ2 = new TotalObserver();
         observ2.doStuff();
