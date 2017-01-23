061e4f65c5d8001e1a867f629bedd98d9292edd7
martin.glas <martin.k.glas@gmail.com>
2017-01-23 22:26:26 +0100

    1: Set up add handler
    Task-Url: https://github.com/martinglas/oida/issues/1

--- oida.bridge/src/oida/bridge/observerservice/changehandler/IChangeHandler.java
+++ oida.bridge/src/oida/bridge/observerservice/changehandler/IChangeHandler.java
@@ -4,27 +4,28 @@ import org.eclipse.emf.common.notify.Notification;
 
 import oida.ontology.manager.IOntologyManager;
 
-/**
- * 
- * @author Michael Shamiyeh, Martin Glas
- * @since 2017-01-20
- *
- */
 public interface IChangeHandler {
-	void setOntologyManager(IOntologyManager manager);
-	
+
 	void handleAdd(Notification notification);
+
 	void handleAddMany(Notification notification);
+
 	void handleMove(Notification notification);
+
 	void handleNoIndex(Notification notification);
-	
+
 	void handleRemove(Notification notification);
+
 	void handleRemoveMany(Notification notification);
-	
+
 	void handleRemovingAdapter(Notification notification);
-	
+
 	void handleResolve(Notification notification);
+
 	void handleSet(Notification notification);
+
 	void handleUnset(Notification notification);
-	
+
+	void setOntologyManager(IOntologyManager owlOntologyManager);
+
 }
