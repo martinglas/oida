785377170016d9d5a49067e15a3a874062f4c332
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-03-10 11:31:18 +0100

    added abstract recommender class;

---
+++ oida.bridge/src/oida/bridge/recommend/AbstractRecommender.java
@@ -0,0 +1,46 @@
+package oida.bridge.recommend;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.util.EContentAdapter;
+
+import oida.ontology.Ontology;
+
+/**
+ * 
+ * @author Michael.Shamiyeh
+ * @since 2017-03-10
+ *
+ */
+public abstract class AbstractRecommender extends EContentAdapter implements IRecommender {
+	private Ontology referenceOntology;
+	private Ontology modelOntology;
+	
+	protected Ontology getReferenceOntology() {
+		return referenceOntology;
+	}
+	
+	protected void setReferenceOntology(Ontology referenceOntology) {
+		this.referenceOntology = referenceOntology;
+	}
+	
+	protected Ontology getModelOntology() {
+		return modelOntology;
+	}
+	
+	protected void setModelOntology(Ontology modelOntology) {
+		this.modelOntology = modelOntology;
+	}
+	
+	protected void connectReferenceOntologyAdapter() {
+		if (this.referenceOntology != null)
+			this.referenceOntology.eAdapters().add(this);
+	}
+	
+	protected void disconnectReferenceOntologyAdapter() {
+		if (this.referenceOntology != null)
+			this.referenceOntology.eAdapters().remove(this);
+	}
+	
+	@Override
+	public abstract void notifyChanged(Notification notification);
+}
