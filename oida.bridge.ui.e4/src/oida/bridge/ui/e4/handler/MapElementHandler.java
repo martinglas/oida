fb721e6ebf93dcc450995ba2144e8e29ece3e166
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-03-17 14:10:57 +0100

    added recommendations view part;

--- oida.bridge.ui.e4/src/oida/bridge/ui/e4/handler/MapElementHandler.java
+++ oida.bridge.ui.e4/src/oida/bridge/ui/e4/handler/MapElementHandler.java
@@ -2,19 +2,24 @@
 package oida.bridge.ui.e4.handler;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+
+import oida.bridge.ui.e4.part.RecommendationsViewPart;
+
 import org.eclipse.e4.core.di.annotations.CanExecute;
 
 public class MapElementHandler {
 	@Execute
 	public void execute() {
-		
+		System.out.println("Do Mapping!");
 	}
-	
 	
 	@CanExecute
-	public boolean canExecute() {
+	public boolean canExecute(@Optional ESelectionService selectionService) {
+		if (selectionService == null || selectionService.getSelection(RecommendationsViewPart.PART_ID) == null)
+			return false;
 		
 		return true;
-	}
-		
+	}	
 }
--- oida.bridge.ui.e4/src/oida/bridge/ui/e4/part/RecommendationsViewPart.java
+++ oida.bridge.ui.e4/src/oida/bridge/ui/e4/part/RecommendationsViewPart.java
@@ -8,21 +8,25 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 
 import bridgemodel.BridgemodelPackage;
 import oida.bridge.service.IOIDABridge;
+import oida.bridge.ui.RecommendationsView.RecommendationsViewInjectorProvider;
 
 public class RecommendationsViewPart {
+	public static final String PART_ID = "oida.bridge.ui.e4.part.oidamappingrecommendations";
+	
 	private TableViewer tableViewer;
 	
 	@Inject
 	IOIDABridge oidaBridge;
 	
-	@Inject
-	ViewerFactory viewerFactory;
-	
 	@PostConstruct
 	public void postConstruct(Composite parent) {
+		Injector injector = RecommendationsViewInjectorProvider.getInjector();
+		ViewerFactory viewerFactory = injector.getInstance(ViewerFactory.class);
+		
 		tableViewer = viewerFactory.createTableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION, BridgemodelPackage.eINSTANCE.getRecommendation());
 
 		tableViewer.setInput(null); //oidaBridge.getCurrentRecommendations());
