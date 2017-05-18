1d26689c6fbb67cfba7b84897ed4b12a62a77d74
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-05-18 17:01:11 +0200

    minor changes;

--- oida.bridge.ui.e4/src/oida/bridge/ui/e4/part/MetaModelClassMappingPart.java
+++ oida.bridge.ui.e4/src/oida/bridge/ui/e4/part/MetaModelClassMappingPart.java
@@ -17,7 +17,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 
 import com.google.inject.Injector;
 
