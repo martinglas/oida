62a32f29dbe0bf8a825aa59da2e5044599fd3822
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2018-04-19 17:05:13 +0200

    deactivated gef-dependent components due to migration issues; changed fragment ids for integration in the eclipse ide;

--- oida.ontology.ui.e4/src/oida/ontology/ui/e4/part/classcontext/ClassContextViewPart.java
+++ oida.ontology.ui.e4/src/oida/ontology/ui/e4/part/classcontext/ClassContextViewPart.java
@@ -1,50 +1,48 @@
-package oida.ontology.ui.e4.part.classcontext;
-
-import javax.annotation.PostConstruct;
-
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
-import org.eclipse.e4.ui.workbench.modeling.ISelectionListener;
-import org.eclipse.gef4.layout.algorithms.SpaceTreeLayoutAlgorithm;
-import org.eclipse.gef4.zest.fx.jface.ZestContentViewer;
-import org.eclipse.gef4.zest.fx.jface.ZestFxJFaceModule;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-
-import com.google.inject.Module;
-
-import oida.ontology.OntologyClass;
-import oida.ontology.ui.e4.part.ClassHierarchyViewPart;
-
-public class ClassContextViewPart implements ISelectionListener {
-	private ZestContentViewer viewer;
-	
-	@PostConstruct
-	public void postConstruct(Composite parent, ESelectionService selectionService) {
-		selectionService.addSelectionListener(ClassHierarchyViewPart.PART_ID, this);
-		
-		ZestFxJFaceModule module = new ZestFxJFaceModule();
-
-		viewer = new ZestContentViewer((Module)module);
-		viewer.createControl(parent, SWT.CENTER);
-		
-		viewer.setLabelProvider(new NodeLabelProvider());
-		viewer.setLayoutAlgorithm(new SpaceTreeLayoutAlgorithm(SpaceTreeLayoutAlgorithm.BOTTOM_UP));
-		//viewer.setLayoutAlgorithm(new TreeLayoutAlgorithm(TreeLayoutAlgorithm.BOTTOM_UP));
-		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			public void selectionChanged(SelectionChangedEvent event) {
-				System.out.println("Selection changed: " + (event.getSelection()));
-			}
-		});
-	}
-
-	@Override
-	public void selectionChanged(MPart part, Object selection) {
-		if (selection instanceof OntologyClass) {
-			viewer.setContentProvider(new GraphContentProvider((OntologyClass)selection));
-			viewer.setInput(selection);
-		}
-	}
-}
+//package oida.ontology.ui.e4.part.classcontext;
+//
+//import javax.annotation.PostConstruct;
+//
+//import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+//import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+//import org.eclipse.e4.ui.workbench.modeling.ISelectionListener;
+//import org.eclipse.gef.layout.algorithms.SpaceTreeLayoutAlgorithm;
+//import org.eclipse.jface.viewers.ISelectionChangedListener;
+//import org.eclipse.jface.viewers.SelectionChangedEvent;
+//import org.eclipse.swt.SWT;
+//import org.eclipse.swt.widgets.Composite;
+//
+//import com.google.inject.Module;
+//
+//import oida.ontology.OntologyClass;
+//import oida.ontology.ui.e4.part.ClassHierarchyViewPart;
+//
+//public class ClassContextViewPart implements ISelectionListener {
+//	private ContentViewer viewer;
+//	
+//	@PostConstruct
+//	public void postConstruct(Composite parent, ESelectionService selectionService) {
+//		selectionService.addSelectionListener(ClassHierarchyViewPart.PART_ID, this);
+//		
+//		ZestFxJFaceModule module = new ZestFxJFaceModule();
+//
+//		viewer = new ZestContentViewer((Module)module);
+//		viewer.createControl(parent, SWT.CENTER);
+//		
+//		viewer.setLabelProvider(new NodeLabelProvider());
+//		viewer.setLayoutAlgorithm(new SpaceTreeLayoutAlgorithm(SpaceTreeLayoutAlgorithm.BOTTOM_UP));
+//		//viewer.setLayoutAlgorithm(new TreeLayoutAlgorithm(TreeLayoutAlgorithm.BOTTOM_UP));
+//		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+//			public void selectionChanged(SelectionChangedEvent event) {
+//				System.out.println("Selection changed: " + (event.getSelection()));
+//			}
+//		});
+//	}
+//
+//	@Override
+//	public void selectionChanged(MPart part, Object selection) {
+//		if (selection instanceof OntologyClass) {
+//			viewer.setContentProvider(new GraphContentProvider((OntologyClass)selection));
+//			viewer.setInput(selection);
+//		}
+//	}
+//}
